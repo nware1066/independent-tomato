6c68992569c7dada2d0f31372c045d42706c9d49
Naomi Ware <nware1066@yahoo.com>
2020-08-26 14:13:47 -0600

    Add basic layout and css to MovieCard component

---
+++ src/MovieCard/MovieCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './movieCard.css'
+
+function MovieCard(props) {
+  return (
+    <article className='movieCard'>
+      <img src={props.img} alt={props.title}/>
+      <p>Title: {props.title}</p>
+      <p>Release Date: {props.releaseDate}</p>
+      <p>Average Rating: {props.averageRating}</p>
+    </article>
+  )
+}
+
+
+export default MovieCard
